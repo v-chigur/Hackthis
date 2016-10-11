555425e4c62fad290a89cd18b6f321fc660bc171
v-chigur <v-chigur@mail.ru>
2016-10-11 20:02:57 +0300

    Should work

--- crypted_bmp_script.py
+++ crypted_bmp_script.py
@@ -14,9 +14,9 @@ bf_res, c_planes, bits = 0, 1, 24
 bh_size, bi_size = 14, 40
 indent = bh_size + bi_size
 compression = 0
-original_bytes= ''.join(bf_type).encode() + fin_size.to_bytes(bf_size, byteorder='big')
-original_bytes += 2 * bf_res.to_bytes(2, byteorder='big') + (indent).to_bytes(4, byteorder='big')
-original_bytes += (bi_size).to_bytes(4, byteorder='big')
+original_bytes= ''.join(bf_type).encode() + fin_size.to_bytes(bf_size, byteorder='little')
+original_bytes += 2 * bf_res.to_bytes(2, byteorder='little') + (indent).to_bytes(4, byteorder='little')
+original_bytes += (bi_size).to_bytes(4, byteorder='little')
 
 x, mx = 4, 4
 while (mx * mx <= fin_size - indent):
@@ -24,12 +24,12 @@ while (mx * mx <= fin_size - indent):
 		x = mx
 	mx += 1
 
-original_bytes += 2 * (x).to_bytes(4, byteorder='big') + (c_planes).to_bytes(2, byteorder='big')
-original_bytes += (bits).to_bytes(2, byteorder='big')
-original_bytes += (compression).to_bytes(4, byteorder='big') + (x * x).to_bytes(4, byteorder='big')
+original_bytes += 2 * (x).to_bytes(4, byteorder='little') + (c_planes).to_bytes(2, byteorder='little')
+original_bytes += (bits).to_bytes(2, byteorder='little')
+original_bytes += (compression).to_bytes(4, byteorder='little') + (x * x).to_bytes(4, byteorder='little')
 
 key = [x ^ y for (x, y) in zip(original_bytes[:KEYLEN], crypted_bytes[:KEYLEN])]
-original_bytes = [(x ^ y).to_bytes(1, byteorder='big') for (x, y) in zip(crypted_bytes, cycle(key))]
+original_bytes = [(x ^ y).to_bytes(1, byteorder='little') for (x, y) in zip(crypted_bytes, cycle(key))]
 
 with open('qr.enc.bmp', 'wb') as fout:
 	for b in original_bytes:
