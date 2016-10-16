d140231714e2368a4f3ca008583badc54f7696de
v-chigur <v-chigur@mail.ru>
2016-10-16 18:02:17 +0300

    Super duper working code

--- torrent.py
+++ torrent.py
@@ -1,44 +1,28 @@
-import bencode, hashlib, struct
-
-MAXBYTE = 256
-bencode_size = 4
-hsh_length = 40
-num_pieces = 24
-
-def get_hash(file_name, piece_length, x):
-	with open(file_name, 'rb') as fin:
-		s = fin.read()
-		if x > 0:
-			chuncks = [s[:x]] + [s[i:i+piece_length] for i in range(x, len(s), piece_length)]
-		else:
-			chuncks = [s[i:i+piece_length] for i in range(x, len(s), piece_length)]
-		hash = ''
-		for chunk in chuncks:
-			hash += hashlib.sha1(chunk).hexdigest()
-	return (chuncks, hash)
+import bencode, hashlib
 
 with open('pincode_torrent.torrent', 'rb') as fin:
 	torrent = bencode.bdecode(fin.read())
 info = torrent['info']
 piece_length = info['piece length']
-pieces = [info['pieces'][i:i+piece_length] for i in range(0, len(info['pieces']), piece_length)]
-hash_pieces = ''.join('%02x' % ord(x) for x in info['pieces'])
 
-lena_tiff, lena_tiff_hsh = get_hash('lena512color.tiff', piece_length, 0)
-x = len(lena_tiff[-1])
-dlt_tiff =  piece_length - x
-dlt_mat = dlt_tiff - bencode_size
+for elem in info['files']:
+	name, size = elem['path'][0], elem['length']
+	if name == 'pincode':
+		piece_length -= size
+		continue
+	if name == 'lena512color.tiff':
+		with open(name, 'rb') as fin:
+			chunk_id = size // piece_length
+			fin.read(chunk_id * piece_length)
+			lena_tiff = fin.read()
+			piece_length -= len(lena_tiff)
+	else:
+		with open(name, 'rb') as fin:
+			lena_mat = fin.read(piece_length)
 
-lena_mat, lena_mat_hsh = get_hash('lena512.mat', piece_length, dlt_mat)
-head = num_pieces * hsh_length
-#print hash_pieces[:head] == lena_tiff_hsh[:head]
-#print hash_pieces[head+hsh_length:] == lena_mat_hsh[hsh_length:]
-torr_hsh = hash_pieces[head:head+hsh_length]
-chunk = [lena_tiff[-1], '', lena_mat[0]]
-
-for i in range(10000):
-	chunk[1] = str(i)
-	h = hashlib.sha1(''.join(chunk)).hexdigest()
-
-	if (torr_hsh == h):
-		print i
+hash = info['pieces'][chunk_id * 20:(chunk_id + 1) * 20]
+i, h = -1, ''
+while h != hash:
+	i += 1
+	h = hashlib.sha1(lena_tiff + str(i) + lena_mat).digest()
+print(i)
